af9b4e1450762b4c52e14c0e6c4e76a01adc60e0
Najim Uddin <engr.najim3@gmail.com>
2022-12-28 16:36:06 +0600

    required tools installation is comleted

---
+++ Adopt Me/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { createRoot } from "react-dom";
+
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.color),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Hello World"),
+    React.createElement(Pet, {
+      name: "doggy",
+      animal: "dog",
+      color: "black",
+    }),
+    React.createElement(Pet, {
+      name: "poggy",
+      animal: "cat",
+      color: "white",
+    }),
+    React.createElement(Pet, {
+      name: "doggy",
+      animal: "caw",
+      color: "red",
+    }),
+  ]);
+};
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
